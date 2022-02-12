212310e17dcbbcbeda35d65ecc9e4b0688aca4fa
skgandikota <sai.gandikota@marks-and-spencer.com>
2022-02-12 06:17:48 +0530

    Added raw link to sitemap

--- index.js
+++ index.js
@@ -3,25 +3,32 @@ const XMLWriter = require('xml-writer');
 const fetch = require("node-fetch");
 
 
-firstRequestToEtrollsApi().then(function (response) {
+firstRequestToEtrollsApi().then(async function (result) {
+    response = result;
+    for (let _i in response) {
+        let { videoId, tags, dialog } = response[_i]
+        res = (await getRawVideo(videoId));
+        response[_i] = Object.assign(response[_i], {link: res.links[0]});
+    }
+    return response;
+}).then(function (response) {
     xw = new XMLWriter;
     xw.startDocument('1.0', 'UTF-8');
     xw.startElement('urlset').writeAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9')
         .writeAttribute("xmlns:image", "http://www.google.com/schemas/sitemap-image/1.1")
         .writeAttribute("xmlns:video", "http://www.google.com/schemas/sitemap-video/1.1");
-
     xw.startElement('url');
     xw.writeElement('loc', 'https://etrolls.in');
     xw.writeElement('lastmod', new Date().toISOString().split('T')[0]);
     xw.writeElement('changefreq', 'daily');
     xw.writeElement('priority', '0.1');
     for (let _i in response) {
-        let { videoId, tags, dialog } = response[_i]
+        let { videoId, tags, dialog, link } = response[_i]
         xw.startElement('video:video')
             .writeElement('video:thumbnail_loc', 'https://img.youtube.com/vi/' + videoId + '/hqdefault.jpg')
             .writeElement('video:title', dialog)
             .writeElement('video:description', "Dailog from the cropped video is `" + dialog + "` and the cropped video is categorized with following " + tags)
-            .writeElement('video:content_loc', "https://etrolls.in/onetroll/yt/" + videoId)
+            .writeElement('video:content_loc', link)
             .writeElement('video:player_loc', "https://www.youtube.com/watch?v=" + videoId)
             .endElement('video:video');
 
@@ -33,7 +40,7 @@ firstRequestToEtrollsApi().then(function (response) {
     }
     xw.endElement('url');
     for (let _i in response) {
-        let { videoId, tags, dialog } = response[_i]
+        let { videoId, tags, dialog, link } = response[_i]
         xw.startElement('url')
             .writeElement('loc', "https://etrolls.in/onetroll/yt/" + videoId)
             .writeElement('lastmod', new Date().toISOString().split('T')[0])
@@ -43,7 +50,7 @@ firstRequestToEtrollsApi().then(function (response) {
             .writeElement('video:thumbnail_loc', 'https://img.youtube.com/vi/' + videoId + '/hqdefault.jpg')
             .writeElement('video:title', dialog)
             .writeElement('video:description', "Dailog from the cropped video is `" + dialog + "` and the cropped video is categorized with following " + tags)
-            .writeElement('video:content_loc', "https://etrolls.in/onetroll/yt/" + videoId)
+            .writeElement('video:content_loc', link)
             .writeElement('video:player_loc', "https://www.youtube.com/watch?v=" + videoId)
             .endElement('video:video');
         xw.startElement('image:image')
@@ -62,6 +69,17 @@ firstRequestToEtrollsApi().then(function (response) {
 
 function firstRequestToEtrollsApi() {
     return fetch('https://api.etrolls.in/youtube/data/getAllRecords.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .catch(function (err) {
+            console.log(`Error: ${err}`)
+        });
+}
+
+
+function getRawVideo(videoId) {
+    return fetch('https://etrolls.herokuapp.com/video_info.php?url=https://www.youtube.com/watch?v=' + videoId)
         .then(function (response) {
             return response.json();
         })
